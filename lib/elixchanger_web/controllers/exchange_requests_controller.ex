5634e78f0fbd41ea2b20ac1af7ad4bf34e13d136
Bruno Tavares <brunotavm@gmail.com>
2024-03-03 20:04:52 -0300

    base listing ExchangeRequest

--- lib/elixchanger_web/controllers/exchange_requests_controller.ex
+++ lib/elixchanger_web/controllers/exchange_requests_controller.ex
@@ -1,7 +1,10 @@
 defmodule ElixchangerWeb.ExchangeRequestsController do
   use ElixchangerWeb, :controller
+  alias Elixchanger.Repo
+  alias Elixchanger.ExchangeRequest
 
   def index(conn, _params) do
-    render(conn, :index)
+    exchange_requests = Repo.all(ExchangeRequest)
+    render(conn, :index, exchange_requests: exchange_requests)
   end
 end
